b50d06d77ca509a4e54820945cea626931ba3de2
zahreddine <zahreddine.atoui@issatso.u-sousse.com>
2021-10-24 19:57:07 +0100

    fix : fix regime null problem

--- Services/ScrapingService.js
+++ Services/ScrapingService.js
@@ -1,14 +1,14 @@
-const axios = require('axios');
-const qs = require('qs');
-const cheerio = require('cheerio');
+const axios = require("axios");
+const qs = require("qs");
+const cheerio = require("cheerio");
 
 function extractToken(html) {
   let $ = cheerio.load(html);
-  return $('#jeton').val();
+  return $("#jeton").val();
 }
 
 function getSessionId(req) {
-  return req.headers['set-cookie'][0].split(';')[0];
+  return req.headers["set-cookie"][0].split(";")[0];
 }
 
 async function loadHtml(url) {
@@ -18,7 +18,7 @@ async function loadHtml(url) {
 
 async function getMajorScheduleHtmlPage(id) {
   const { htmlPage, req } = await loadHtml(
-    'http://www.issatso.rnu.tn/fo/emplois/emploi_groupe.php'
+    "http://www.issatso.rnu.tn/fo/emplois/emploi_groupe.php"
   );
 
   const token = extractToken(htmlPage);
@@ -31,10 +31,10 @@ async function getMajorScheduleHtmlPage(id) {
   });
 
   const config = {
-    method: 'post',
-    url: 'http://www.issatso.rnu.tn/fo/emplois/emploi_groupe.php',
+    method: "post",
+    url: "http://www.issatso.rnu.tn/fo/emplois/emploi_groupe.php",
     headers: {
-      'Content-Type': 'application/x-www-form-urlencoded',
+      "Content-Type": "application/x-www-form-urlencoded",
       Cookie: phpSessionIdCookie,
     },
     data: data,
@@ -48,9 +48,9 @@ function extractScheduleFromThePage(html) {
   const $ = cheerio.load(html);
   const result = [];
   // table can be null => throw error
-  const scheduleTable = $('#dvContainer > table > tbody > tr');
+  const scheduleTable = $("#dvContainer > table > tbody > tr");
   if (!scheduleTable.length) {
-    throw new Error('Schedule scrapping failed');
+    throw new Error("Schedule scrapping failed");
   }
   scheduleTable.each(function () {
     let row = [];
@@ -58,55 +58,51 @@ function extractScheduleFromThePage(html) {
       .children()
       .each(function () {
         let cell = $(this).text().trim();
-        if (cell.length > 0) {
-          row.push(cell);
-        }
+        row.push(cell);
       });
-    if (row.length > 0) {
-      result.push(row);
-    }
+    result.push(row);
   });
-
+  result.shift();
   return result;
 }
 
 function parseExtractedDataToJson(schedule) {
-  let subGroup = '1';
-  let day = '';
+  let subGroup = "1";
+  let day = "";
   const refactoredSchedule = { 1: {}, 2: {} };
 
   let initializedSessions = [];
-  schedule.shift();
+
   schedule.forEach((row) => {
     if (row[0].match(/.*-.*-2/)) {
-      subGroup = '2';
+      subGroup = "2";
       refactoredSchedule[subGroup] = JSON.parse(
-        JSON.stringify(refactoredSchedule['1'])
+        JSON.stringify(refactoredSchedule["1"])
       );
     } else if (row[0].match(/^[123456]-/)) {
       day = row[0];
-      if (subGroup === '1') refactoredSchedule[subGroup][day] = {};
+      if (subGroup === "1") refactoredSchedule[subGroup][day] = {};
     } else {
-      const isSessionEmpty = !refactoredSchedule[subGroup][day][row[0]];
+      const isSessionEmpty = !refactoredSchedule[subGroup][day][row[1]];
 
       if (
         isSessionEmpty ||
-        (subGroup === '2' && !initializedSessions.includes(row[0] + day))
+        (subGroup === "2" && !initializedSessions.includes(row[1] + day))
       ) {
-        refactoredSchedule[subGroup][day][row[0]] = [];
-        if (subGroup === '2') {
-          initializedSessions.push(row[0] + day);
+        refactoredSchedule[subGroup][day][row[1]] = [];
+        if (subGroup === "2") {
+          initializedSessions.push(row[1] + day);
         }
       }
       let session = {
-        start: row[1],
-        end: row[2],
-        desc: row[3],
-        type: row[4],
-        classroom: row[5],
-        regime: row[6],
+        start: row[2],
+        end: row[3],
+        desc: row[4],
+        type: row[5],
+        classroom: row[6],
+        regime: row[7],
       };
-      refactoredSchedule[subGroup][day][row[0]].push(session);
+      refactoredSchedule[subGroup][day][row[1]].push(session);
     }
   });
   return refactoredSchedule;
@@ -124,14 +120,14 @@ const getScheduleByMajorId = async function (majorId) {
 
 const getAllMajors = async function () {
   const { htmlPage } = await loadHtml(
-    'http://www.issatso.rnu.tn/fo/emplois/emploi_groupe.php'
+    "http://www.issatso.rnu.tn/fo/emplois/emploi_groupe.php"
   );
   const $ = cheerio.load(htmlPage);
   const majors = [];
   const majorList = $(
-    '#form1 > table > tbody > tr > td:nth-child(2) > select > option'
+    "#form1 > table > tbody > tr > td:nth-child(2) > select > option"
   );
-  if (!majorList.length) throw new Error('error getting majors list .');
+  if (!majorList.length) throw new Error("error getting majors list .");
   majorList.each(function () {
     majors.push({ id: $(this).val(), label: $(this).text() });
   });
@@ -140,16 +136,16 @@ const getAllMajors = async function () {
 
 const getScheduleValidity = async () => {
   const { htmlPage } = await loadHtml(
-    'http://www.issatso.rnu.tn/fo/emplois/emploi_groupe.php'
+    "http://www.issatso.rnu.tn/fo/emplois/emploi_groupe.php"
   );
   const $ = cheerio.load(htmlPage);
   // schedule validity example : à partir de: 19-10-2021
   const scheduleValidity = $(
-    'body > div.wrapper > div > div > div > div.row > article > div > center:nth-child(1) > table > tbody > tr:nth-child(1) > td > center > h5'
+    "body > div.wrapper > div > div > div > div.row > article > div > center:nth-child(1) > table > tbody > tr:nth-child(1) > td > center > h5"
   )
     .text()
     .trim();
-  return scheduleValidity.split(':')[1].trim();
+  return scheduleValidity.split(":")[1].trim();
 };
 
 module.exports = { getAllMajors, getScheduleByMajorId, getScheduleValidity };
